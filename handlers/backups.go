f25c21a44f1ddc7ab1994adf0830ed75e6d53f8f
Pavel Milanov <milanov71@icloud.com>
2024-09-03 14:43:29 +0300

    рефакторинг

--- handlers/backups.go
+++ handlers/backups.go
@@ -38,24 +38,12 @@ func (h *Handler) backupHandler(c *gin.Context) {
 	backupTime := json.SelectedTime
 	backupCron := json.SelectedCron
 
-	log.Println(dbname, backupRun, backupCount, backupTime, backupCron)
 	if backupRun == db.BACKUP_RUN[1] && (backupCount == "" || backupCron == "" || backupTime == "") {
 		c.JSON(http.StatusOK, gin.H{
 			"error": "расписание не может быть пустым",
 		})
 		return
 	}
-
-	// curBackups := db.CheckBackup(db.BACKUP_DIR)
-	// for _, item := range curBackups {
-	// 	if strings.Contains(item, backupName) {
-	// 		errStr := fmt.Sprintf("Бекап %s уже существует!", backupName)
-	// 		c.JSON(http.StatusOK, gin.H{
-	// 			"error": errStr,
-	// 		})
-	// 		return
-	// 	}
-	// }
 	newBackup, err := db.CreateBackup(*h.CONFIG, dbname, backupRun, backupCount, backupTime, backupCron)
 	if err != nil {
 		log.Fatal(err)
@@ -64,23 +52,6 @@ func (h *Handler) backupHandler(c *gin.Context) {
 		})
 		return
 	}
-
-	// newBackup := db.Backup{
-	// 	Alias:    dbname,
-	// 	Date:     currTime,
-	// 	Size:     backupData["size"],
-	// 	LeadTime: backupData["time"],
-	// 	Status:   backupData["status"],
-	// 	Schedule: db.BackupSchedule{
-	// 		Run:   backupRun,
-	// 		Count: backupCount,
-	// 		Time:  backupTime,
-	// 		Cron:  backupCron,
-	// 	},
-	// }
-
-	// backupsInfo := db.CreateBackupData(&newBackup, db.BACKUPDATA_DIR)
-	// log.Println(backupsInfo)
 	c.JSON(http.StatusOK, gin.H{
 		"message": newBackup,
 	})
