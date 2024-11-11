a09bf1e017b11417c7021c0509fb18aa49b01042
pavel_milanov <milanov71@icloud.com>
2024-11-11 22:15:12 +0300

    Revert "устранение багов"
    This reverts commit 86c68b5f7d5cde46632926cb7471761ebca6bfd3.

--- static/js/main.js
+++ static/js/main.js
@@ -6,24 +6,3 @@ window.onload = function () {
         console.log('jQuery is not loaded')
     }
 }
-
-$(document).ready(function () {
-    const ctx = $('#storageChart')[0].getContext('2d')
-
-    const used = parseFloat($('#storageChart').data('used'))
-    const total = parseFloat($('#storageChart').data('total'))
-    new Chart(ctx, {
-        type: 'doughnut',
-        data: {
-            labels: ['Использовано', 'Свободно'],
-            datasets: [{
-                data: [used, total],
-                backgroundColor: ['#1e3c72', '#e0e0e0']
-            }]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    })
-})
